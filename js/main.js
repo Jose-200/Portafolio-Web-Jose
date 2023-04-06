c426a702f0a9597dc5502270fccdc3729cea9824
Jose Fuilco <jfuilcogal@gmail.com>
2023-04-05 19:33:05 -0500

    Add navegator

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Variable globales
+let show = false
+// constantes del DOM
+const menu = document.getElementById("menu")
+const view = document.getElementById("menu-view")
+const path = document.querySelectorAll("a")
+
+// Recorrido forEach para poder manipular los id del querySelectorAll
+path.forEach((item) => {
+    item.onclick = () => {
+        // Recorrido forEach para poder desactivar todos
+        path.forEach((elem) => elem.className = "inactive" )
+
+        // Activo el boton que corresponde
+        item.className = "active"
+
+        // Desactivo el show
+        if(show){
+            view.classList.add("hidden")
+            setTimeout(() => {
+                view.classList.remove("hidden")
+                view.classList.remove("show")
+                show = false
+            }, 1000)
+        }
+    }
+})
+
+// Mostrar el menu
+menu.onclick = () => {
+    if (!show){
+        view.classList.add("show")
+        show = true
+    } else {
+        view.classList.add("hidden")
+        setTimeout(() => {
+            view.classList.remove("hidden")
+            view.classList.remove("show")
+            show = false
+        }, 1000)
+    }
+}
