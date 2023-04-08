bb4f684a2c50e6c730404f8c1091b930a4958435
Jose Fuilco <jfuilcogal@gmail.com>
2023-04-07 19:05:54 -0500

    Grid alineado

--- public/js/main.js
+++ public/js/main.js
@@ -8,14 +8,19 @@ const path = document.querySelectorAll("a")
 // Recorrido forEach para poder manipular los id del querySelectorAll
 path.forEach((item) => {
     item.onclick = () => {
-        // Recorrido forEach para poder desactivar todos
-        if(item.id != "continue"){
+        // Recorrido forEach para poder desactivar todos y activar el correspondido
+        if(item.id != "continue" && item.id != "moreabout"){
             path.forEach((elem) => elem.className = "inactive" )
             item.className = "active"
         } else if (item.id == "continue") {
             path.forEach((elem) => elem.className = "inactive" )
             path.forEach((elem) => {
                 if(elem.id == "vtec") { elem.className = "active" }
+            })
+        } else if (item.id == "moreabout") {
+            path.forEach((elem) => elem.className = "inactive" )
+            path.forEach((elem) => {
+                if(elem.id == "vabo") { elem.className = "active" }
             })
         }
 
